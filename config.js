fb2822a3f986289b7b257baa68109d6d16eb7db1
KeerthanaS <keerukeerthana8@gmail.com>
2017-10-07 22:05:47 +0530

    Add config

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.page_token = "EAARAeZAXo8XkBACgulgF1EZAWs0UZCmDm3xMmMAU4rr467OIdwijry7dd8iHdWMMK6gIJsLVQqULNCWGI04MMfd8rgcZAJMEcvg9eSd5rZCA75t5dXSChkgrjbiDHFcyKyl2XFWtX1GYpMzsOL3QkssWcekzw15GY4cvTZASDCGUf1RHZBjbUOR";
+config.verify_token = "TOKEN";
+
+module.exports = config;
