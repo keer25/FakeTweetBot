f6fdff13add34b7173bb03aefdaae6967aad6101
KeerthanaS <keerukeerthana8@gmail.com>
2017-10-08 05:57:32 +0530

    Game responses

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ verify_token: config.verify_token
 // Setup Express middleware for /webhook
 app.use("/webhook", bot.middleware());
 
+var payloadMessages = [ { "payload": "GUESSED_FAKE_FAKE", "message": "Correct! You have quite an eye" },
+						{ "payload": "GUESSED_FAKE_NOTFAKE", "message": "Ouch! It is actually a real tweet." },				
+						{ "payload": "GUESSED_NOTFAKE_FAKE", "message": "You have been fooled. It is Fake. You are out user demographic" },
+						{ "payload": "GUESSED_NOTFAKE_NOTFAKE", "message": "Yup it is not fake.. Clever!" }
+					];
+
 // Setup listener for incoming messages
 bot.on('message', function(userId, message){
 bot.sendTextMessage(userId, "I can't process that");
@@ -18,8 +24,8 @@ bot.sendTextMessage(userId, "I can't process that");
 
 bot.on('attachment', function(userId, attachment){
 	if(attachment[0].type == "image") {
-		bot.sendImageMessage(userId, attachment[0].payload.url);
-		//Replace this with th call to the detector API
+		//Replace this with th call to the detector API and store attachment[0].payload.url
+		bot.sendTextMessage(userId, "Analyser under construction");
 	}else {
 		bot.sendTextMessage(userId, "I can't process that");
 	}
@@ -39,6 +45,37 @@ bot.setPersistentMenu(menuButtons);
 bot.on('postback', function(userId, payload){
     if (payload == "GET_STARTED") {
         getStarted(userId);
+    }else if (payload == 'PLAY_GAME') {
+    	//Get a random image url from the API and whether it is fake or not
+    	url = "https://scontent.fmaa1-1.fna.fbcdn.net/v/t34.0-0/p280x280/22360975_175571383007543_665586497_n.jpg?oh=369b2cda7ae6014c72a5846af6a0640b&oe=59DBFBF6";
+    	isFake = true;
+    	offset = 0;
+    	if(!isFake) {
+    		offset = 1;
+    	}
+    	bot.sendImageMessage(userId, url);
+    	var text = "Guess if the tweet is fake or not";
+		var buttons = [
+		    {
+		        "type": "postback",
+		        "title": "Fake",
+		        "payload": payloadMessages[offset].payload,
+		        "fake": "no"
+		    },
+		    {
+		        "type": "postback",
+		        "title": "Not Fake",
+		        "payload": payloadMessages[2 + offset].payload,
+		        "fake": "no"
+		    }
+	];
+		bot.sendButtonMessage(userId, text, buttons);
+    }else {
+    	for (i = 0;i < 4; i++){
+    		if(payload == payloadMessages[i].payload) {
+    			bot.sendTextMessage(userId, payloadMessages[i].message);
+    		}
+    	}
     }
 });
 
